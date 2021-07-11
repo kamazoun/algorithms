585b5db46d2ac28a2d8fc4525b5d7024793e475d
kamazoun <44663772+kamazoun@users.noreply.github.com>
2021-07-11 22:15:10 +0200

    Add files via upload

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -7,7 +7,7 @@ def insertion_sort(arr):
     for j in range(1, len(arr)):
         key = arr[j]
         i = j - 1
-        while i >= 0 and arr[i] > key:
+        while i >= 0 and arr[i] > key: # from key at position i, will move `up` one notch all the arr[i] > key, 0 <= i < k, and insert key at the position of the last arr[i] > key (which by now would be at i+1)  
             arr[i+1] = arr[i]
             i -= 1
         arr[i+1] = key
