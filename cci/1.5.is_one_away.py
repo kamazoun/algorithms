784ee1d790f9bfe3f1975219591eb593be73653d
kamazoun <44663772+kamazoun@users.noreply.github.com>
2021-07-14 13:03:02 +0200

    Add files via upload

---
+++ cci/1.5.is_one_away.py
@@ -0,0 +1,54 @@
+R'''
+There are three types of edits that can be performed on strings: insert a character,
+remove a character, or replace a character. Given two strings, write a function to check if they are
+one edit (or zero edits) away.
+EXAMPLE
+pale, ple -> true
+pales, pale -> true
+pale, bale -> true
+pale, bake -> false
+'''
+
+def is_one_away(a, b):
+    R'''
+    O(n)time and space.
+    The challenge in that implementation is to be careful with the branching conditions. Logically it's simple, but it can be confusing.
+    If we reach the end of string b, yet not the end of a, there is 1 (and only one, the first check makes sure we have one unit of length difference at most and the [diff] variable would return if two differences were already encountered) more char in a, so one more difference. We make sure that there were no other differences before that one.
+    '''
+    if abs(len(a) - len(b)) > 1:
+        return False
+
+    j = 0
+    diff = 0
+    for i in range(len(a)):
+        if j >= len(b):
+            return diff + 1 <= 1
+        
+        if a[i] == b[j]: # Normal case. Char are the same
+            j += 1
+            continue # Not needed, just to make clear
+
+        elif a[i+1] == b[j]: # A char has been inserted in a at index i
+            diff += 1
+
+        elif a[i] == b[j+1]: # A char has been inserted in b at index i
+            diff += 1
+            j += 2
+
+        elif a[i+1] == b[j+1]: # A char has been replaced in one of the strings at index i
+            diff += 1
+            j += 1
+
+        if diff > 1:
+            return False
+
+    return True
+
+
+#print(url_ify_in_place(['1', ' ', '\t', '2', '', '', '', '', '', ''], 4))
+print(is_one_away('pale', 'ple'))
+print(is_one_away('pales', 'pale'))
+print(is_one_away('pale', 'bale'))
+print(is_one_away('pale', 'bake'))
+print(is_one_away('ple', 'pale'))
+print(is_one_away('ple', 'pal'))
---
+++ cci/1.6string_compression.py
@@ -0,0 +1,41 @@
+R'''
+Implement a method to perform basic string compression using the counts
+of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the
+"compressed" string would not become smaller than the original string, your method should return
+the original string. You can assume the string has only uppercase and lowercase letters (a - z)
+'''
+
+def string_compression(s):
+    R'''
+    O(n) time of course.
+    O(n) space too: The main thing here is that in python the tuples being init'ed the way they are, I need to use the index to print the values to not get and IndexOutOfBounds exception.
+    Alternatively, we could init compressed as [(0, 0)]*len(s) and use for (x, y) in compressed: print(x,y,sep). Still the values with 0 would appear, bringing us back to using indexing.
+    '''
+    compressed  = [()]*len(s)
+    gains = 0 # The number of chars we get by compressing s
+    i, order = 0, 0
+    
+    while i < len(s):
+        j = i + 1
+        while j < len(s) and s[j] == s[i]:
+            j += 1
+        consecutive_letters = j - i
+        compressed[order] = (s[i], consecutive_letters)
+        order += 1
+        gains += consecutive_letters - 2
+        i = j
+
+    if gains > 0:
+        for i in range(((len(s)-gains-1)//2)): # To avoid printing empty elements
+            x = compressed[i][0]
+            y = compressed[i][1]
+            print(x,y, sep = '', end='')
+
+    else:
+        print(s)
+
+
+    
+#print(url_ify_in_place(['1', ' ', '\t', '2', '', '', '', '', '', ''], 4))
+print(string_compression('aabcccccaaa'))
+print(string_compression('asdfghjkl'))
