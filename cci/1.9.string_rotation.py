a62a737a0470c4d62589dfd57976756e57ef7154
kamazoun <kamazoun@hotmail.fr>
2023-10-15 22:28:48 +0000

    Solved cci/1.9 with hint from author and added it to interesting_problems

--- cci/1.9.string_rotation.py
+++ cci/1.9.string_rotation.py
@@ -6,8 +6,24 @@ call to isSubstring (e.g.,"waterbottle" is a rotation of"erbottlewat")
 
 
 def is_rotation(s1: str, s2: str) -> bool:
+    R"""
+    We use a hint from the author:
+    What happens when we concatenate s1 or s2 with itself.
+    :param s1: One of the strings
+    :param s2: The other string
+    :return: A boolean indicating whether s1 and s2 are rotations of each other.
+    """
     def is_substring(full_string: str, substring: str) -> bool:
-        if len(substring) < len(full_string):  # Not necessary
+        if len(substring) > len(full_string):  # Not necessary
             return False
         return substring in full_string
-    pass
+    if is_substring(s2*2, s1):
+        return True
+    return False
+
+
+s1 = 'waterbottle'
+s2 = 'erbottlewat'
+
+r = is_rotation(s1, s2)
+print(r)
