cc983590a4bddcfee78b6b368a1bf2374faef68c
kamazoun <kamazoun@hotmail.fr>
2023-10-20 23:58:00 +0000

    draft for one linked list implementation

--- cci/2.1.remove_dups.py
+++ cci/2.1.remove_dups.py
@@ -1,14 +1,14 @@
-R'''
+R"""
 Write code to remove duplicates from an unsorted linked list.
 FOLLOW UP
 How would you solve this problem if a temporary buffer is not allowed?
-'''
+"""
 
 def remove_dups_with_buffer(head_node):
-    R'''
+    R"""
     Implementing this in python might not be as intuitive as it would in c/c++.
     We assume the nodes are references. Also we used a dict instead of a hash_table.
-    '''
+    """
     n = head_node
 
     dict = {}
