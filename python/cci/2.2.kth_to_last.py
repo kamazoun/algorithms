6705ec939cbd3f1524d0f7f3613e3cd9bdd9d699
kamazoun <kamazoun@gmail.com>
2024-05-05 19:23:00 +0000

    started work on cci/2.2

--- python/cci/2.2.kth_to_last.py
+++ python/cci/2.2.kth_to_last.py
@@ -2,8 +2,16 @@ R'''
  Implement an algorithm to find the kth to last element of a singly linked list.
 '''
 
+class Node:
+    def __init__(self):
+        next = None
 
-
+def kth_to_last_new(head: Node, k: int) -> Node:
+    R"""
+    The first idea I have here is to have a runner move k position before starting to move the head node.
+    We need to pay attention of edge cases (such as when the list is less that k element long).
+    """
+    pass
 def kth_to_last(head, k):
     R'''
     O(n) time and O(1) space.
