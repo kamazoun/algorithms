4a5001f012d6bdfa5743d320fa63f2b92ca9c406
kamazoun <kamazoun@gmail.com>
2024-12-15 22:37:59 +0000

    cci/2.5: continuation

--- python/cci/2.5.sum_lists.py
+++ python/cci/2.5.sum_lists.py
@@ -27,8 +27,41 @@ def sum_lists(head1: Node, head2: Node) -> Node:
             
     return head_r
 
+
+
+def primary_case(h1: Node, h2: Node) -> Node:
+    if not h1 or not h2:
+        return h2 if not h1 else h1
+    
+    r = 0 # Retenue
+    h3 = None
+    while h1 or h2:
+        if h1 and h2:
+            v = h1.data + h2.data + r
+            d = v % 10
+            r = v // 10
+            n = Node(d)
+            if h3:
+                h3.next = n
+                h3 = h3.next
+            else:
+                h3 = n
+        else:
+            if h1:
+                h3.next = Node(h1.data)
+                h1 = h1.next
+            elif h2:
+                h3.next = Node(h2.data)
+                h2 = h2.next
+            h3 = h3.next
+    return h3
+
+
+
+# ============= TESTS ================>
+
 a = [1, 2, 3]
-b = [4, 5, 6]
+b = [4, 5]
 
 head_a = None
 node = None
@@ -53,6 +86,12 @@ for i in b:
 
 
 head_r = sum_lists(head_a, head_b)
+
+while head_r:
+    print(head_r.data, end= " ---> ")
+    head_r = head_r.next
+
+head_r = primary_case(head_a, head_b)
 
 while head_r:
     print(head_r.data, end= " ---> ")
