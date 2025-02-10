40ca841cd1b6abc073094adafba5995ff17b6607
kamazoun <kamazoun@gmail.com>
2025-02-10 21:06:18 +0000

    Initialize module structure and implement is_palindrome function stub

---
+++ python/cci/chapter2/6/is_palindrome.py
@@ -0,0 +1,45 @@
+# my_python_project/python/cci/problem2/6is_palindrome.py
+import sys
+import os
+# Step 1: Determine the current script's directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# Step 2: Determine the project root directory
+# Adjust the number of '../' based on your projectstructure
+project_root = os.path.abspath(os.path.join(current_dir, '../../..'))
+print(project_root)
+# Step 3: Add the project root to sys.path if it'snot already there
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+# Step 4: Perform the import using absolute paths
+from cci.chapter2.node import Node
+def is_palindrome(head: Node) -> bool:
+    pass
+    # values = []
+    # current = head
+    # while current:
+    #     values.append(current.value)
+    #     current = current.next
+    # return values == values[::-1]
+
+
+if __name__ == "__main__":
+    # Example Usage
+    node1 = Node(1)
+    node2 = Node(2)
+    node3 = Node(1)
+    node1.next = node2
+    node2.next = node3
+    print(is_palindrome(node1))  # Output: True
+
+
+
+
+
+
+
+
+# from cci.chapter2.node import Node
+ 
+
+# def is_palindrome(head: Node) -> bool:
+#     print('fewr')
