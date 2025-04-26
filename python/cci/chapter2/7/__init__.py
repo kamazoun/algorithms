248c4cb22a0759fdc1e9fcbb44774da59e413145
kamazoun <kamazoun@gmail.com>
2025-04-26 11:06:53 +0000

    Implement authors function to find the intersection node of two linked lists by reference

--- python/cci/chapter2/7/__init__.py
+++ python/cci/chapter2/7/__init__.py
@@ -27,6 +27,46 @@ else:
 # Step 4: Perform the import using absolute paths
 from cci.chapter2.node import Node
 
+
+def authors(head1: Node, head2: Node):
+    """
+    The solution involves finding the first identical node in the two linked lists (by reference).
+    To account for the difference in length, we can traverse the longer list first and then traverse the shorter list. This way, both pointers will be at the same distance from the end of the lists when they meet.
+    """
+    def length(head: Node) -> int:
+        count = 0
+        last = None
+        while head:
+            count += 1
+            last = head
+            head = head.next
+        return count, last
+    
+    if not head1 or not head2:
+        return None
+    
+    len1, last1 = length(head1)
+    len2,last2 = length(head2)
+    if last1 is not last2: # If the last nodes are different, the lists do not intersect
+        return None
+    
+    # Move the pointer of the longer list ahead by the difference in lengths
+    if len1 > len2:
+        for _ in range(len1 - len2):
+            head1 = head1.next
+    else:
+        for _ in range(len2 - len1):
+            head2 = head2.next
+    # Traverse both lists together until the intersection point is found
+    while head1 and head2:
+        if head1 is head2:
+            return head1
+        head1 = head1.next
+        head2 = head2.next
+    return None
+
+
+
 def intersection(head1: Node, head2: Node) -> Node:
     """
     Using Flyoyd's Tortoise and Hare algorithm to find the intersection point of two linked lists."""
