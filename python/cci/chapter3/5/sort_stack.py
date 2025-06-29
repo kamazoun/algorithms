3ccfee466d506a209a3626b9654753f3c3c5e31e
kamazoun <kamazoun@gmail.com>
2025-06-29 23:21:43 +0000

    Enhance SortStack class with additional methods and detailed docstring for sorting functionality

--- python/cci/chapter3/5/sort_stack.py
+++ python/cci/chapter3/5/sort_stack.py
@@ -1,5 +1,8 @@
 """
-
+Sort Stack: Write a program to sort a stack such that the smallest items are on the top. You can use
+an additional temporary stack, but you may not copy the elements into any other data structure
+(such as an array). The stack supports the following operations: push, pop, peek, and is Empty.
+Hints:# 15, #32, #43
 """
 
 class SortStack:
@@ -13,4 +16,17 @@ class SortStack:
         self.stack.append(item)
         while temp_stack:
             self.stack.append(temp_stack.pop())
+
+    def pop(self):
+        if self.stack:
+            return self.stack.pop()
+        raise IndexError("pop from empty stack")
+
+    def peek(self):
+        if self.stack:
+            return self.stack[-1]
+        raise IndexError("peek from empty stack")   
+
+    def is_empty(self):
+        return len(self.stack) == 0
 
